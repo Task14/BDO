a637a0b7b526fcc02230d83e4830d6c18e1dbb01
Task14 <task14_93@hotmail.com>
2017-11-30 16:19:22 +0200

    Bug Fixes In Pagination

--- BDO/settings.py
+++ BDO/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'userrequests.apps.UserrequestsConfig',
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
